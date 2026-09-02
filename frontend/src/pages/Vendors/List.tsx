4c87ca22ee15e40a298186abe554a954b7e0c201
Vibin <cybervibz@gmail.com>
2026-09-03 03:33:39 +0530

    fixed a lot of small bugs

--- frontend/src/pages/Vendors/List.tsx
+++ frontend/src/pages/Vendors/List.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Card, Button, TextInput, ListLoadingState, ListEmptyState, StatusBadge } from '../../components/ui';
 import { cardErrorPadded } from '../../styles/classNames';
